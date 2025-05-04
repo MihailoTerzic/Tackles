4be512463060c6bc2cd967590e8675d0321ed6d7
Terzo <mihailoterzic035@gmail.com>
2025-05-04 09:21:07 +0200

    fixed text responsiveness on TopHero component

--- components/TopHero.js
+++ components/TopHero.js
@@ -7,7 +7,7 @@ export default function TopHero({img,name}) {
   return (
     <section className="relative h-[50vh] bg-cover bg-center flex items-end " style={{ backgroundImage: `url(${img})` }}>
     <div className="w-full px-20 mx-auto  text-white pb-20 bg-gradient-to-t from-black/60 to-transparent">
-      <h1 className="text-6xl font-bold">{name}</h1>
+      <h1 className="text-4xl md:text-6xl font-bold">{name}</h1>
       <div className="text-sm mt-2 text-end">
         <a href="/" className="">Home</a> <span className="mx-2">/</span> {name}
       </div>
